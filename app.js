e9cafb2c24d42b53927884b9099c90b4019e74dd
Alex Samih-Zade <galutziogen@gmail.com>
2024-10-02 12:08:39 +0300

    bypass all

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-import processor from "./src/services/inews-service.js";
-import routes from "./src/routes/routes.js";
+//import processor from "./src/services/inews-service.js";
+//import routes from "./src/routes/routes.js";
 import express from "express";
 import cors from "cors";
 import getServerIP from "./src/utilities/host-ip.js";
 import bodyParser from 'body-parser';
-import logger from "./src/utilities/logger.js";
+//import logger from "./src/utilities/logger.js";
 const app = express(); 
 
-app.use(cors({origin: '*'}));
+//app.use(cors({origin: '*'}));
 
 // Increase payload size limit to 50MB
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+//app.use(bodyParser.json({ limit: '50mb' }));
+//app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 //app.use("/api",routes);
 
 // Static server http://localhost:3000/plugin
-app.use(express.static('plugin')); 
+//app.use(express.static('plugin')); 
 
 // Start the Express server
 const port = 3000;
