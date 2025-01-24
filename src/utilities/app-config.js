6125eb062ab14bf8de55e931c2cea788d1ce36ab
Alex Samih-Zade <galutziogen@gmail.com>
2025-01-24 20:10:17 +0300

    1.1.1 version fix

--- src/utilities/app-config.js
+++ src/utilities/app-config.js
@@ -2,6 +2,6 @@ import { readFileSync } from 'fs';
 
 const appConfig =JSON.parse(readFileSync('./config.json', 'utf8'));
 
-appConfig.version = "1.1.0";
+appConfig.version = "1.1.1";
 
 export default appConfig;
